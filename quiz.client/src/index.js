49fecc75abb9ccaf3915136abf40bb7a825d1c49
Ricardo Rodriguez <tjrprecursor@gmail.com>
2021-02-13 00:50:31 +0100

    adapt index app file

--- quiz.client/src/index.js
+++ quiz.client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import GameApp from './GameApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GameApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
